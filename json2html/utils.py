dcee6a79f1654661dc74f35c300c0678276b401a
brookite <brookit18@gmail.com>
2023-09-19 11:30:20 +0300

    Added whitespace parameter to Tab class

--- json2html/utils.py
+++ json2html/utils.py
@@ -1,9 +1,12 @@
 class Tab:
     """Immutable HTML indentiation consisting of 4*level non-breaking spaces (&nbsp;)."""
 
-    def __init__(self, level: int):
+    def __init__(self, level: int, whitespaces=4):
         if level < 0:
             level = 0
+        if whitespaces <= 0:
+            whitespaces = 1
+        self._whitespaces = whitespaces
         self._level = level
 
     def up(self):
@@ -11,6 +14,12 @@ class Tab:
 
     def down(self):
         return Tab(self._level - 1)
+
+    def set_level(self, level: int):
+        self._level = level
+
+    def set_whitespaces(self, whitespaces: int):
+        self._whitespaces = whitespaces
 
     def __bool__(self):
         return self._level != 0
